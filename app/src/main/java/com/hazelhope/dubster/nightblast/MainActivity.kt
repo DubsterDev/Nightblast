a7e5b76cb17b807d084c0bae0d4dd21fb378b3f7
DubsterDev <317384959+DubsterDev@users.noreply.github.com>
2026-09-05 22:30:04 -0500

    Add ime padding

--- app/src/main/java/com/hazelhope/dubster/nightblast/MainActivity.kt
+++ app/src/main/java/com/hazelhope/dubster/nightblast/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.selection.selectable
@@ -257,7 +258,7 @@ fun SendMessage(sendMessage: (phoneNumber: String, message: String, priority: In
     }
 
     Column(
-        modifier = modifier.padding(12.dp).verticalScroll(rememberScrollState()),
+        modifier = modifier.imePadding().padding(12.dp).verticalScroll(rememberScrollState()),
         verticalArrangement = Arrangement.spacedBy(12.dp)
     ) {
         Text(
