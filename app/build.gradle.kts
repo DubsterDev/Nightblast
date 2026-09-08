976bd9a89f91a112b08a2ab3b0c5e1fef949ea58
DubsterDev <317384959+DubsterDev@users.noreply.github.com>
2026-09-08 18:31:15 -0500

    Remove dependency info signing block

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.hazelhope.dubster.nightblast"
         minSdk = 24
         targetSdk = 37
-        versionCode = 101
-        versionName = "1.0.1"
+        versionCode = 102
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -34,6 +34,11 @@ android {
     }
     buildFeatures {
         compose = true
+    }
+
+    dependenciesInfo {
+        includeInApk = false
+        includeInBundle = false
     }
 }
 
