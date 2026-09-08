25f78f73e93089488ea2850993bbd16db928f54d
DubsterDev <317384959+DubsterDev@users.noreply.github.com>
2026-09-08 18:19:38 -0500

    Remove foojay, bump version to 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.hazelhope.dubster.nightblast"
         minSdk = 24
         targetSdk = 37
-        versionCode = 100
-        versionName = "1.0.0"
+        versionCode = 101
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
